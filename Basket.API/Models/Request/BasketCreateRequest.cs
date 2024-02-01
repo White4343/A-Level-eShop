81133725cf990d07a7e12a0709f07fe4879236e6
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-02-01 17:10:49 +0200

    Feat - Added Models

---
+++ Basket.API/Models/Request/BasketCreateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Basket.API.Models.Request
+{
+    public class BasketCreateRequest
+    {
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal ItemPrice { get; set; }
+
+        [Required]
+        public int ItemId { get; set; }
+    }
+}
---
+++ Basket.API/Models/Request/BasketUpdateRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Basket.API.Models.Request
+{
+    public class BasketUpdateRequest
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal ItemPrice { get; set; }
+
+        [Required]
+        public int ItemId { get; set; }
+    }
+}
