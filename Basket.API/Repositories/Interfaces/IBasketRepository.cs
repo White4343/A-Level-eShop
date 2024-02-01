124a47bdbaf1f29c3f49a5ada85325cb9414d111
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-02-01 17:08:49 +0200

    Feat - added repository for CRUD and additional functions

---
+++ Basket.API/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Repositories.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<BasketDto> CreateBasketAsync(Data.Entities.Basket basket);
+
+        Task<IEnumerable<BasketDto>> GetBasketsAsync();
+
+        Task<IEnumerable<BasketDto>> GetBasketByLoginAsync(string login);
+
+        Task<BasketDto> GetBasketByIdAsync(int id, string login);
+
+        Task<BasketDto> UpdateBasketAsync(Data.Entities.Basket basket, string login);
+        
+        Task<bool> DeleteBasketByIdAsync(int id, string login);
+
+        Task<bool> DeleteBasketByLoginAsync(string login);
+    }
+}
