79362d6f7159955df23383131bb4cd9e1f710d86
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-02-01 17:06:17 +0200

    Feat - initial creation of Basket.API

---
+++ Basket.API/Program.cs
@@ -0,0 +1,159 @@
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+using Basket.API.Services;
+using Basket.API.Services.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace Basket.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = GetConfiguration();
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            var authority = configuration["IdentityServer:Authority"];
+
+            builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = authority;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+
+                    options.RequireHttpsMetadata = false;
+                });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Basket.FullAccess", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "basket.fullaccess");
+                });
+            });
+
+            // Add services to the container.
+
+            builder.Services.AddAutoMapper(typeof(Program));
+
+            builder.Services.AddTransient<IBasketRepository, BasketRepository>();
+            builder.Services.AddTransient<IBasketService, BasketService>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows()
+                    {
+                        Implicit = new OpenApiOAuthFlow()
+                        {
+                            AuthorizationUrl = new Uri($"{authority}/connect/authorize"),
+                            TokenUrl = new Uri($"{authority}/connect/token"),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { "basket.fullaccess", "Basket API" }
+                            }
+                        }
+                    }
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Id = "oauth2",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new[] { "basket.fullaccess" }
+                    }
+                });
+            });
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionString")));
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.SetIsOriginAllowed((host => true))
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(setup =>
+                {
+                    setup.SwaggerEndpoint($"{configuration["PathBase"]}/swagger/v1/swagger.json", "Basket.API v1");
+                    setup.OAuthClientId("basketswaggerui");
+                    setup.OAuthAppName("Basket Swagger UI");
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            CreateDbIfNotExists(app);
+            app.Run();
+
+            IConfiguration GetConfiguration()
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", false, true)
+                    .AddEnvironmentVariables();
+
+                return builder.Build();
+            }
+
+            void CreateDbIfNotExists(IHost host)
+            {
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    try
+                    {
+                        var context = services.GetRequiredService<AppDbContext>();
+
+                        DbInnit.InnitAsync(context).Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        var logger = services.GetRequiredService<ILogger<Program>>();
+                        logger.LogError(ex, "An error occurred creating the DB.");
+                    }
+                }
+            }
+        }
+    }
+}
