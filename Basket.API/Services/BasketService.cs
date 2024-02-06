7a13a44303106c12397d5676fd37b88d93bdac6a
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-02-06 17:20:23 +0200

    Feat: added CatalogService integration

--- Basket.API/Services/BasketService.cs
+++ Basket.API/Services/BasketService.cs
@@ -1,32 +1,51 @@
 ﻿using Basket.API.Repositories.Interfaces;
 using Basket.API.Services.Interfaces;
-using Basket = Basket.API.Data.Entities.Basket;
 
 namespace Basket.API.Services
 {
     public class BasketService : IBasketService
     {
         private readonly IBasketRepository _basketRepository;
+        private readonly ICatalogService _catalogService;
         private readonly ILogger<BasketService> _logger;
         private readonly IMapper _mapper;
 
-        public BasketService(IBasketRepository basketRepository, ILogger<BasketService> logger, IMapper mapper)
+        public BasketService(IBasketRepository basketRepository, ICatalogService catalogService, ILogger<BasketService> logger, IMapper mapper)
         {
             _basketRepository = basketRepository;
+            _catalogService = catalogService;
             _logger = logger;
             _mapper = mapper;
         }
 
         public async Task<BasketDto> CreateBasketAsync(BasketCreateRequest basket, string login)
         {
+            var item = await IsItemAvailable(basket.ItemId, basket.Quantity);
+
+            if (!item)
+            {
+                _logger.LogError("Item not available.");
+
+                return null;
+            }
+
             var basketEntity = new Data.Entities.Basket
             {
-                CreatedAt = DateTime.Now,
+                CreatedAt = DateTime.UtcNow,
                 Quantity = basket.Quantity,
                 ItemPrice = basket.ItemPrice,
                 ItemId = basket.ItemId,
                 UserLogin = login
             };
+
+            var itemQuantity = await PatchItemQuantity(basket.ItemId, basket.Quantity);
+
+            if (itemQuantity == false)
+            {
+                _logger.LogError("Item not available.");
+
+                return null;
+            }
 
             return await _basketRepository.CreateBasketAsync(basketEntity);
         }
@@ -48,6 +67,15 @@ namespace Basket.API.Services
 
         public async Task<BasketDto> UpdateBasketAsync(BasketUpdateRequest basket, string login)
         {
+            var item = await IsItemAvailable(basket.ItemId, basket.Quantity);
+
+            if (!item)
+            {
+                _logger.LogError("Item not available.");
+
+                return null;
+            }
+
             var basketEntity = new Data.Entities.Basket
             {
                 Id = basket.Id,
@@ -56,6 +84,15 @@ namespace Basket.API.Services
                 ItemId = basket.ItemId,
                 UserLogin = login
             };
+
+            //var itemQuantity = await PatchItemQuantity(basket.ItemId, basket.Quantity);
+
+            //if (itemQuantity == false)
+            //{
+            //    _logger.LogError("Item not available.");
+
+            //    return null;
+            //}
 
             return await _basketRepository.UpdateBasketAsync(basketEntity, login);
         }
@@ -68,6 +105,20 @@ namespace Basket.API.Services
         public async Task<bool> DeleteBasketByLoginAsync(string login)
         {
             return await _basketRepository.DeleteBasketByLoginAsync(login);
+        }
+
+        private async Task<bool> IsItemAvailable(int itemId, int quantity)
+        {
+            var item = await _catalogService.GetItemByIdAsync(itemId);
+
+            return item != null && item.AvailableStock >= quantity;
+        }
+
+        private async Task<bool> PatchItemQuantity(int itemId, int quantity)
+        {
+            var item = await _catalogService.PatchItemQuantityAsync(itemId, quantity);
+
+            return item;
         }
     }
 }
