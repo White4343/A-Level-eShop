99fd071b7d3db1190e5a978c9bd8e54a6e67c706
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-02-06 17:18:18 +0200

    Fix: fixed mapping

--- Basket.API/Mapping/MapperProfile.cs
+++ Basket.API/Mapping/MapperProfile.cs
@@ -5,7 +5,7 @@
         public MapperProfile()
         {
             CreateMap<Data.Entities.Basket, BasketDto>();
-            CreateMap<Data.Entities.Basket, BasketDto>();
+            CreateMap<BasketDto, Data.Entities.Basket> ();
 
             CreateMap<BasketCreateRequest, Data.Entities.Basket>();
             CreateMap<Data.Entities.Basket, BasketCreateRequest>();
