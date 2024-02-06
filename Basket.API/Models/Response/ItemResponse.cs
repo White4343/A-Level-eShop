f3c42c2293614f387f32dba4ef72d373c9c19d72
Андрій Тарасенко <andrii.tarasenko@nure.ua>
2024-02-06 17:18:48 +0200

    Feat: added ItemResponse record

---
+++ Basket.API/Models/Response/ItemResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Models.Response
+{
+    public record ItemResponse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public int AvailableStock { get; set; }
+
+        public int TypeId { get; set; }
+
+        public int BrandId { get; set; }
+    }
+}
